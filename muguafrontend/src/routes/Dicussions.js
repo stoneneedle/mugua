1797f6e22db1f67ce72f8d64d83ffb0524da4a8e
stoneneedle <stoneneedle@gmail.com>
2022-06-07 20:21:14 -0400

    Added discussion post interface MUI HTML fields.

--- muguafrontend/src/routes/Dicussions.js
+++ muguafrontend/src/routes/Dicussions.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, FormControl, TextField, TextareaAutosize } from "@mui/material";
 
 export default class Discussions extends React.Component {
   render() {
@@ -14,6 +15,32 @@ export default class Discussions extends React.Component {
     return (
       <div>
         <h2>Discussions</h2>
+        <FormControl>
+          <table>
+            <tr align="right">
+              <td>
+                <TextField id="standard-basic" label="Title" variant="standard" style={{ width: 400 }}/>
+              </td>
+            </tr>
+            <tr align="right">
+              <td>
+                <TextareaAutosize
+                  aria-label="minimum height"
+                  minRows={10}
+                  placeholder="Thread message"
+                  style={{ width: 400 }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Button variant="contained">Post</Button>
+              </td>
+            </tr>
+          </table>
+        </FormControl>
+
+
         <p>{postsStr}</p>
       </div>
     );
